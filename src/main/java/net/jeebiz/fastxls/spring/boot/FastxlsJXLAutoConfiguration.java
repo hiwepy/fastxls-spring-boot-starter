ecb1fab43257d3e6f737fc89769d94b2027c98c4
wandl <hnxyhcwdl1003@163.com>
2019-11-20 18:06:56 +0800

    修改

--- src/main/java/net/jeebiz/fastxls/spring/boot/FastxlsJXLAutoConfiguration.java
+++ src/main/java/net/jeebiz/fastxls/spring/boot/FastxlsJXLAutoConfiguration.java
@@ -1,14 +1,13 @@
 package net.jeebiz.fastxls.spring.boot;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.biz.context.NestedMessageSource;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import net.jeebiz.fastxls.core.provider.def.DefaultValidationMessageProvider;
+import net.jeebiz.fastxls.jexcel.JXLWorkbookFiller;
+import net.jeebiz.fastxls.jexcel.JXLWorkbookMapper;
+import net.jeebiz.fastxls.jexcel.JXLWorkbookReader;
 import net.jeebiz.fastxls.spring.boot.property.FastxlsExportProperties;
 import net.jeebiz.fastxls.spring.boot.property.FastxlsImportProperties;
 import net.jeebiz.fastxls.spring.boot.property.FastxlsStoreProperties;
@@ -18,15 +17,21 @@ import net.jeebiz.fastxls.spring.boot.property.FastxlsValidationProperties;
 @ConditionalOnProperty(prefix = FastxlsProperties.PREFIX, value = "enabled", havingValue = "true")
 @EnableConfigurationProperties({ FastxlsProperties.class, FastxlsStoreProperties.class,
 		FastxlsImportProperties.class, FastxlsExportProperties.class, FastxlsValidationProperties.class })
-public class FastxlsAutoConfiguration {
+public class FastxlsJXLAutoConfiguration  {
+	
+	@Bean
+	public JXLWorkbookFiller jxlWorkbookFiller() {
+		return new JXLWorkbookFiller();
+	}
 
 	@Bean
-	@ConditionalOnMissingBean
-	public DefaultValidationMessageProvider validationMessageProvider(
-			@Autowired(required = false) NestedMessageSource messageSource) {
-		return new DefaultValidationMessageProvider(messageSource);
+	public JXLWorkbookMapper jxlWorkbookMapper() {
+		return new JXLWorkbookMapper();
 	}
-	
-	
+
+	@Bean
+	public JXLWorkbookReader jxlWorkbookReader() {
+		return new JXLWorkbookReader();
+	}
 
 }
---
+++ src/main/java/net/jeebiz/fastxls/spring/boot/FastxlsPOIAutoConfiguration.java
@@ -0,0 +1,61 @@
+package net.jeebiz.fastxls.spring.boot;
+
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import net.jeebiz.fastxls.poi.POIWorkbookConverter;
+import net.jeebiz.fastxls.poi.POIWorkbookCopyer;
+import net.jeebiz.fastxls.poi.POIWorkbookExtractor;
+import net.jeebiz.fastxls.poi.POIWorkbookFiller;
+import net.jeebiz.fastxls.poi.POIWorkbookMapper;
+import net.jeebiz.fastxls.poi.POIWorkbookReader;
+import net.jeebiz.fastxls.poi.POIWorkbookWriter;
+import net.jeebiz.fastxls.spring.boot.property.FastxlsExportProperties;
+import net.jeebiz.fastxls.spring.boot.property.FastxlsImportProperties;
+import net.jeebiz.fastxls.spring.boot.property.FastxlsStoreProperties;
+import net.jeebiz.fastxls.spring.boot.property.FastxlsValidationProperties;
+
+@Configuration
+@ConditionalOnProperty(prefix = FastxlsProperties.PREFIX, value = "enabled", havingValue = "true")
+@EnableConfigurationProperties({ FastxlsProperties.class, FastxlsStoreProperties.class,
+		FastxlsImportProperties.class, FastxlsExportProperties.class, FastxlsValidationProperties.class })
+public class FastxlsPOIAutoConfiguration {
+
+	@Bean
+	public POIWorkbookConverter poiWorkbookConverter() {
+		return new POIWorkbookConverter();
+	}
+	
+	@Bean
+	public POIWorkbookCopyer poiWorkbookCopyer() {
+		return new POIWorkbookCopyer();
+	}
+	
+	@Bean
+	public POIWorkbookExtractor poiWorkbookExtractor() {
+		return new POIWorkbookExtractor();
+	}
+	
+	@Bean
+	public POIWorkbookFiller poiWorkbookFiller() {
+		return new POIWorkbookFiller();
+	}
+
+	@Bean
+	public POIWorkbookMapper poiWorkbookMapper() {
+		return new POIWorkbookMapper();
+	}
+
+	@Bean
+	public POIWorkbookReader poiWorkbookReader() {
+		return new POIWorkbookReader();
+	}
+	
+	@Bean
+	public POIWorkbookWriter poiWorkbookWriter() {
+		return new POIWorkbookWriter();
+	}
+
+}
